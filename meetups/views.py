f5b97e4f86d7cfac30d99a8973bf1ac83cf9a115
yoel <rjuanyoel@gmail.com>
2021-10-19 12:18:11 -0400

    primer uso de plantillas

--- meetups/views.py
+++ meetups/views.py
@@ -4,5 +4,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {'title': 'A First Meetup'},
+        {'title': 'A Second Meetup'}
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
 
