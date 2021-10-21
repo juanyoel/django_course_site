d7459943a6caf5c2053bc3a284569adf14d35980
yoel <rjuanyoel@gmail.com>
2021-10-21 09:35:10 -0400

    continuaciopn de relaciones entre tablas
    presentacion de esos datos en la web

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-from . models import Meetup
+from . models import Meetup, Location, Participant
 # Register your models here.
 
 
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    list_filter = ('email',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location', 'address')
+    list_filter = ('location',)
+
+
 class MeetupAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug')
-    list_filter = ('title',)
+    list_display = ('title', 'date', 'location')
+    list_filter = ('location', 'date')
     prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Participant)
+
+
 
